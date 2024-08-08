e5ee477f45fa83a58b2d3c1e24f2afc6dd6feb12
Majeedat Abdulwahab <majeedatabdulwahabayo@gnail.com>
2024-08-08 12:07:17 +0100

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header /> 
+       
         {children}
       </body>
     </html>
