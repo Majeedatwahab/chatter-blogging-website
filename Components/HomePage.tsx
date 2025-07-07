d24a9676146af49602e00d3fd53c9ee0ad59a463
Majeedat Abdulwahab <majeedatabdulwahabayo@gnail.com>
2025-07-07 18:18:49 +0100

    Added Authentication right back!!!

--- Components/HomePage.tsx
+++ Components/HomePage.tsx
@@ -23,12 +23,20 @@ const HomePage = () => {
         <p className="text-lg mb-8 text-black font-bold">
           Elevate Your Voice - Where Every Word Matters.
         </p>
-        <Link
-          href="/dashboard"
-          className="bg-emerald-700 text-white px-4 py-2 rounded hover:bg-black"
-        >
-          Go to Dashboard
-        </Link>
+        <div className="flex space-x-4 justify-center">
+          <Link
+            href="/login"
+            className="bg-emerald-700 text-white px-4 py-2 rounded hover:bg-black"
+          >
+            Sign In
+          </Link>
+          <Link
+            href="/signup"
+            className="bg-black text-white px-4 py-2 rounded hover:bg-emerald-700"
+          >
+            Sign Up
+          </Link>
+        </div>
       </div>
     </div>
   );
